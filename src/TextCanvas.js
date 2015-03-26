7453c1853beb7d41091332dc35b9eb1dabadd040
jbreckmckye <jimmy@breck-mckye.com>
2015-03-26 21:51:01 +0000

    Removing some objects in prep for refactor

--- src/TextCanvas.js
+++ src/TextCanvas.js
@@ -3,36 +3,49 @@ module.exports = TextCanvas;
 var LineSet = require('./LineSet.js');
 var util = require('./util.js');
 
-function TextCanvas(paper, x, y, width, height, lineHeight) {
-	height = height ? height : Infinity;
+function TextCanvas(paper, x, y, lineHeight) {
+	var that = this;
+	var lines = paper.set();
+	var nextLineY = y;
 
-	var rightmostX = x + width; // What about RTL?
-	var bottommostY = y + height;
-
-	var lineSet = new LineSet(paper, x, y, lineHeight);
-	var lineSetStates = [];
-
-	this.addTextToLine = lineSet.addTextToLine;
-
-	this.addNewLine = lineSet.addNewLine;
-
-	this.areBoundsBroken = function areBoundsBroken() {
-		var boundingBox = lineSet.getBBox();
-		return (boundingBox.x2 > rightmostX || boundingBox.y2 > bottommostY);
+	function removeAllLines() {
+		lines.remove();
+		nextLineY = y;
 	};
 
-	this.saveState = function saveState() {
-		var lineSetCurrentState = lineSet.getLineTexts();
-		lineSetStates.push(lineSetCurrentState);
+	this.addLine = function addLine() {
+		var newLine = paper.text(x, nextLineY, '');
+		lines.push(newLine);
+		nextLineY += lineHeight;
 	};
 
-	this.rollbackState = function rollbackState() {
-		var lastState = util.arrayLast(lineSetStates);
-		lineSet.createLines(lastState);
+	this.addTextToLine = function addTextToLine(text) {
+		var currentLine = util.arrayLast(lines);
+		var currentText = getText(currentLine);
+		var newText = currentText + text;
+		setText(currentLine, newText);
 	};
 
-	this.addNewLine();
-	this.saveState();
+	this.createLines = function createLines(lineTexts) {
+		removeAllLines();
+		util.arrayForEach(lineTexts, function(lineText){
+			that.addLine();
+			that.addTextToLine(lineText);
+		});
+	};
+
+	this.getBBox = function getBBox() {
+		return lines.getBBox();
+	};
+
+	this.getLineTexts = function getLineTexts() {
+		var texts = [];
+		lines.forEach(function(line){
+			var lineText = getText(line);
+			texts.push(lineText);
+		});
+		return texts;
+	};
 }
 
 function setText(element, text) {
--- src/paragraph.js
+++ src/paragraph.js
@@ -2,25 +2,9 @@ module.exports = paragraph;
 
 // Dependencies
 var extractWordsFromText = require('./extractWordsFromText.js');
-//var TextCanvas = require('./TextCanvas.js');
+var TextCanvas = require('./TextCanvas.js');
 
 function paragraph(x, y, text, width, height, lineHeight) {
 	var paper = this;
 	var words = extractWordsFromText(text);
-	//var textCanvas = new TextCanvas(paper, x, y, width, height);
-	//addWordsToTextCanvasWithoutBreakingBounds(words, textCanvas);
 }
-
-// var wordAdditionStrategies = {
-// 	firstWord : [justAddWord],
-// 	nthWord : [addSpaceThenWord, addLineBreakThenWord]
-// };
-
-// function addWordsToTextCanvasWithoutBreakingBounds(words, textCanvas) {
-// 	var remainingWords = words.slice();
-
-// 	while (thereAreStillWords() && thereIsStillSpace()) {
-// 		addWordUsingFirstWorkingStrategy();
-// 		remainingWords.shift();
-// 	}
-// }
