7f05c5e49e19e79c4cd42e5000717612e9d42160
jbreckmckye <jimmy@breck-mckye.com>
2015-03-26 00:04:24 +0000

    Removed duplicate LineSet definition

--- src/TextCanvas.js
+++ src/TextCanvas.js
@@ -35,64 +35,6 @@ function TextCanvas(paper, x, y, width, height, lineHeight) {
 	this.saveState();
 }
 
-function LineSet(paper, x, y, lineHeight) {
-	var that = this;
-	var lines = paper.set();
-	var nextLineY = y;
-
-	function removeAllLines() {
-		lines.remove();
-		nextLineY = y;
-	};
-
-	this.addLine = function addLine() {
-		var newLine = paper.text(x, nextLineY, '');
-		lines.push(newLine);
-		nextLineY += lineHeight;
-	};
-
-	this.addTextToLine = function addTextToLine(text) { // should we include a /n guard here?
-		var currentLine = util.arrayLast(lines);
-		var currentText = getText(currentLine);
-		var newText = currentText + text;
-		setText(currentLine, newText);
-	};
-
-	this.createLines = function createLines(lineTexts) {
-		removeAllLines();
-		util.arrayForEach(lineTexts, function(lineText){
-			that.addLine();
-			that.addTextToLine(lineText);
-		});
-	};
-
-	this.getBBox = function getBBox() {
-		return lines.getBBox();
-	};
-
-	this.getLineTexts = function getLineTexts() {
-		var texts = [];
-		lines.forEach(function(line){
-			var lineText = getText(line);
-			texts.push(lineText);
-		});
-		return texts;
-	};
-
-}
-
-
-
-function arrayForEach(array, fn) { // Shimmed for IE8
-	for (var i = 0; i < array.length; i++) {
-		fn(array[i], i, array);
-	}
-}
-
-function arrayLast(array) {
-	return array[array.length - 1];
-}
-
 function setText(element, text) {
 	element.attr('text', newText);
 }
